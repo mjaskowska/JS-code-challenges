ebf7c485a745fb0a330bfe987ed4377bd4c97e6e
Magda <jaskowska.magda@gmail.com>
2020-11-11 13:56:24 +0000

    multiplication table with numbers from 1 to 10

---
+++ challenge8.js
@@ -0,0 +1,7 @@
+// Print all the multiplication tables with numbers from 1 to 10
+
+for (j = 0; j <= 10; j++) {
+    for (i = 0; i <= 10; i++) {
+        console.log(i + " * " + j + " = " + i * j)
+    }
+}
